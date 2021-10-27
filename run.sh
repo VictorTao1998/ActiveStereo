01d1b44a07f3ccfe32a3c10945ad02634d0dd0c6
VictorTao1998 <jit124@ucsd.edu>
2021-10-26 17:05:44 -0700

    init

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ export PYTHONWARNINGS="ignore"
 now=$(date +"%Y%m%d_%H%M%S")
 model_name="StereoGAN"
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -u /jianyu-fast-vol/StereoGAN/train.py \
---logdir='/jianyu-fast-vol/eval/StereoGAN_train_1' \
---config-file '/jianyu-fast-vol/StereoGAN/configs/remote_train_gan.yaml'
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -u /jianyu-fast-vol/ActiveStereo/main.py \
+--logdir='/jianyu-fast-vol/eval/ActiveStereo_train' \
+--config-file '/jianyu-fast-vol/ActiveStereo/configs/remote_train_gan.yaml'
 --summary-freq 50
